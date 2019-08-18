562a3471e75ea992ccc56bc97716011dfc1d837c
Leif Liddy <leif.liddy@gmail.com>
2019-08-19 00:16:37 +0200

    corrected minor errors

--- install.cirrus.driver.sh
+++ install.cirrus.driver.sh
@@ -41,10 +41,10 @@ ls -lA $update_dir
 pulse_daemon_conf='/etc/pulse/daemon.conf'
 [[ ! -f $pulse_daemon_conf ]] && exit
 
-default_sample_channels=$(egrep '^default-sample-channels.*$' $pulse_daemon_conf)
+default_sample_channels=$(grep '^default-sample-channels.*$' $pulse_daemon_conf)
 
 if [ -n "$default_sample_channels" ]; then
-   if [ "$default_sample_channels" != 'default_sample_channels = 4' ]; then
+   if [[ "$default_sample_channels" != 'default_sample_channels = 4' ]]; then
 	  echo -e "\nmodifying /etc/pulse/daemon.conf\ndefault-sample-channels = 4"
 	  sed -i 's/^default-sample-channels.*$/default-sample-channels = 4/' $pulse_daemon_conf
 	  killall pulseaudio &> /dev/null
