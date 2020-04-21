1cb256ec47bfc6517e32581b2873f4ffe88cacdf
Leif Liddy <leif.liddy@gmail.com>
2020-04-21 03:15:38 +0200

    corrected error

--- install.cirrus.driver.sh
+++ install.cirrus.driver.sh
@@ -35,7 +35,11 @@ cp $patch_dir/Makefile $patch_dir/patch_cirrus.c $patch_dir/patch_cirrus_a1534_s
 # if kernel version is < 5.6 then change
 # timespec64 to timespec
 # ktime_get_real_ts64 to getnstimeofday
-[[ $major_minor -lt 56 ]] && sed -i 's/timespec64/timespec/' $hda_dir/patch_cirrus.c && sed -i 's/ktime_get_real_ts64/getnstimeofday/' $hda_dir/patch_cirrus_a1534_pcm.h
+if $major_minor -lt 56; then
+   sed -i 's/timespec64/timespec/' $hda_dir/patch_cirrus.c
+   sed -i 's/timespec64/timespec/' $hda_dir/patch_cirrus_a1534_pcm.h
+   sed -i 's/ktime_get_real_ts64/getnstimeofday/' $hda_dir/patch_cirrus_a1534_pcm.h
+fi
 
 cd $hda_dir
 make
